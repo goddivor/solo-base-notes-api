aea40633c97c49523ed44a2517d1e444177cc232
Divor <todjropatrice2002@gmail.com>
2025-10-17 17:25:04 +0200

    feat: implement collaborative platform (remove userId filters)
    - Remove userId filters from all GET queries
    - Allow all authenticated users to view and modify all data
    - Keep userId tracking for audit purposes
    - Update environment variables example

--- models/User.js
+++ models/User.js
@@ -18,6 +18,9 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
   },
+  youtubeChannelUrl: {
+    type: String,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
